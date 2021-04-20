ac1c45b1f7099dd639d3d766269fd28a0994bc02
JouryE <jourye@gmail.com>
2021-04-20 21:14:25 +0200

    huiswerk aan het maken

--- gamelogic.js
+++ gamelogic.js
@@ -1,9 +1,34 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
-}
+  let displayedWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    console.log("????:", word[i])
+    // variable met letter
+    const letter = word[i];
+    // zit die in de guesses array?
+    const isGuessed = guesses.includes(letter);
+    if (isGuessed) {
+      const toAdd = letter + " ";
+      displayedWord += toAdd;
+    } else {
+      displayedWord += "_ ";
+    }
+  }
+
+  return displayedWord;
+  }
+
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  if (guesses) {
+    const guesses =! word;
+  }
+  return false;
+  if (guesses) {
+    const guesses = word;
+  }
+  return true;
+
 }
 
 function isGameLost(word, guesses) {
